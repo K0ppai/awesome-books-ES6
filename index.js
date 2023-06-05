42479663b052c32dddafb816f3ca4e12fb627120
koppai <psthu.koppai@gmail.com>
2023-06-05 12:46:34 +0630

    Add date

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // import BookListApp from './modules/BookListApp.js';
+import { DateTime } from './modules/luxon.js';
 import * as GenerateSec from './modules/GenerateSec.js';
 import {
   handleFormSubmit, handleRemoveButtonClick, form, ul,
@@ -9,3 +10,6 @@ GenerateSec.addForm.addEventListener('click', GenerateSec.generateAddForm);
 GenerateSec.contact.addEventListener('click', GenerateSec.generateContact);
 form.addEventListener('submit', handleFormSubmit);
 ul.addEventListener('click', handleRemoveButtonClick);
+
+const date = document.querySelector('#time');
+date.innerText = `${DateTime.now().toLocaleString(DateTime.DATE_MED)}, ${DateTime.now().toLocaleString(DateTime.TIME_WITH_SECONDS)}`;
